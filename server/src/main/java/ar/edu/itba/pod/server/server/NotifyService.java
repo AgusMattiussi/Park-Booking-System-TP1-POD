09a66231712b4c7c6e8b4d8e57c2703192531a21
Juli <sassojulian@gmail.com>
2023-09-13 11:45:35 -0300

    Rename NotifyServer to NotifyService

--- server/src/main/java/ar/edu/itba/pod/server/server/NotifyService.java
+++ server/src/main/java/ar/edu/itba/pod/server/server/NotifyService.java
@@ -2,7 +2,6 @@ package ar.edu.itba.pod.server.server;
 
 
 import ar.edu.itba.pod.server.persistance.RideRepository;
-import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +9,10 @@ import rideBooking.Models;
 import rideBooking.NotifyServiceGrpc;
 import rideBooking.NotifyServiceOuterClass;
 import rideBooking.NotifyServiceOuterClass.NotifyRequest;
-import rideBooking.NotifyServiceOuterClass.Notification;
 
 import java.util.UUID;
 
-public class NotifyServer extends NotifyServiceGrpc.NotifyServiceImplBase{
+public class NotifyService extends NotifyServiceGrpc.NotifyServiceImplBase{
 
     private final static Logger logger = LoggerFactory.getLogger(Server.class);
     private static final RideRepository repository = RideRepository.getInstance();
