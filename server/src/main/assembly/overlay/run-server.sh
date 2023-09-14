dea19b51ac863a57f0c815a855626f11752dcdc8
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-09-14 01:04:58 -0300

    Add missing execution scripts

--- server/src/main/assembly/overlay/run-server.sh
+++ server/src/main/assembly/overlay/run-server.sh
@@ -4,7 +4,7 @@ PATH_TO_CODE_BASE=`pwd`
 
 #JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"	
 
-MAIN_CLASS="ar.edu.itba.pod.grpc.server.Server"
+MAIN_CLASS="ar.edu.itba.pod.server.Server"
 
 
 java  $JAVA_OPTS -cp 'lib/jars/*' $MAIN_CLASS $*
