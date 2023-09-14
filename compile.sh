e5d4d1ccd50354b09ec3fc0a91c66f5327628545
Agustin Mattiussi <amattiussi@itba.edu.ar>
2023-09-14 01:26:08 -0300

    Refactor compile.sh

--- compile.sh
+++ compile.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+target_dir="server/target"
+client_dir="client/target"
+temp_dir="tmp"
+
 mvn clean install
 
-mkdir tmp
+mkdir -p "$temp_dir"
 
-cp server/target/tpe1-g6-server-2023.2Q-bin.tar.gz ./tmp/tpe1-g6-server-2023.2Q-bin.tar.gz
-cp client/target/tpe1-g6-client-2023.2Q-bin.tar.gz ./tmp/tpe1-g6-client-2023.2Q-bin.tar.gz
+cp "$target_dir/tpe1-g6-server-2023.2Q-bin.tar.gz" "$temp_dir/"
+cp "$client_dir/tpe1-g6-client-2023.2Q-bin.tar.gz" "$temp_dir/"
+cd "$temp_dir"
 
-cd tmp
-
-tar -xzf tpe1-g6-server-2023.2Q-bin.tar.gz
-tar -xzf tpe1-g6-client-2023.2Q-bin.tar.gz
-
+# Server
+tar -xzf "tpe1-g6-server-2023.2Q-bin.tar.gz"
 chmod +x tpe1-g6-server-2023.2Q/*.sh
 sed -i -e 's/\r$//' tpe1-g6-server-2023.2Q/*.sh
-rm tpe1-g6-server-2023.2Q-bin.tar.gz
+rm "tpe1-g6-server-2023.2Q-bin.tar.gz"
 
+# Client
+tar -xzf "tpe1-g6-client-2023.2Q-bin.tar.gz"
 chmod +x tpe1-g6-client-2023.2Q/*.sh
 sed -i -e 's/\r$//' tpe1-g6-client-2023.2Q/*.sh
-rm tpe1-g6-client-2023.2Q-bin.tar.gz
+rm "tpe1-g6-client-2023.2Q-bin.tar.gz"
 
